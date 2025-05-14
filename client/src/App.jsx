9df5755936bbd29af74c2d607233a40eea208386
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-05-14 20:23:47 +0530

    gradient colors added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import BuyCredit from "./pages/BuyCredit";
 const App = () => {
   return (
     <>
-      <div className="px-4, sm:px-10">
+      <div className="px-4, sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-100 to-orange-100">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/result" element={<Result />} />

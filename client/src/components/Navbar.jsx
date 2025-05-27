fa86ab311eb65d9221d7356a335db695920bbe0f
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-05-27 19:51:56 +0530

    navbar updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { React, useState } from "react";
+import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [user, setUser] = useState(null);
+
+  return (
+    <>
+      <div>
+        <Link to="/">
+          <img src={assets.logo} alt="" className="w-25 sm:w-32 lg:w-40" />
+        </Link>
+      </div>
+
+      <div>
+        {user ? (
+          <div className="div"></div>
+        ) : (
+          <div className="div">
+            <p>Pricing</p>
+            <button>Login</button>
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Navbar;

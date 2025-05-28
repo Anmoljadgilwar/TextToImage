1393d287faa2adb02137bab0e3704c785fbe3a3e
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-05-28 18:10:22 +0530

    initial commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Header = () => {
+  return (
+    <div className="flex flex-col justify-center items-center text-center my-20 ">
+      <div className="text-stone-500 flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500">
+        <p>Best AI Tool for your Image Genaration</p>
+        <img src={assets.star_icon} alt="" />
+      </div>
+
+      <h1 className="text-4xl max-w-[300px] sm:text-5xl sm:max-w-[600px] mx-auto mt-10 text-center font-normal">
+        Turn Your Text into Stunning{" "}
+        <span className="text-blue-500">Images</span>
+      </h1>
+      <p className="text-center max-w-xl mx-auto mt-5">
+        Unleash your creativity with AI. Turn your imagination into visual art
+        in seconds - with just a few words.
+      </p>
+    </div>
+  );
+};
+
+export default Header;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,27 +1,59 @@
-import { React, useState } from "react";
+import { useContext } from "react";
 import { assets } from "../assets/assets";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext.jsx";
 
 const Navbar = () => {
-  const [user, setUser] = useState(null);
+  const user = useContext(AppContext);
+
+  const navigate = useNavigate();
 
   return (
     <>
-      <div>
-        <Link to="/">
-          <img src={assets.logo} alt="" className="w-25 sm:w-32 lg:w-40" />
-        </Link>
-      </div>
+      <div className="flex">
+        <div className="mr-auto">
+          <Link to="/">
+            <img src={assets.logo} alt="" className="w-25 sm:w-32 lg:w-40" />
+          </Link>
+        </div>
 
-      <div>
-        {user ? (
-          <div className="div"></div>
-        ) : (
-          <div className="div">
-            <p>Pricing</p>
-            <button>Login</button>
-          </div>
-        )}
+        <div>
+          {user ? (
+            <div className="flex items-center gap-2 sm:gap-3">
+              <button
+                onClick={() => navigate("/buy")}
+                className="flex items-center gap-2 sm:gap-3 bg-blue-100 px-4 sm:px-6 py1.5 sm:py-3 rounded-full transition-all duration-700 hover:scale-105 "
+              >
+                <img alt="" src={assets.credit_star} className="w-5 " />
+                <p>credit left: 108</p>
+              </button>
+              <p className="text-gray-600 pl-4 max-sm:hidden "> Hi, Anmol</p>
+              <div className="relative group ">
+                <img
+                  src={assets.profile_icon}
+                  className="w-10 drop-shadow"
+                  alt=""
+                />
+                <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12">
+                  <ul>
+                    <li className=" bg-violet-100 rounded-b-sm cursor-pointer p-1">
+                      logout
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="flex items-center gap-2 sm:gap-5">
+              <p onClick={() => navigate("/buy")} className="cursor-pointer ">
+                Pricing
+              </p>
+              <button className="bg-zinc-800 text-white py-2 px-7 rounded-full sm:px-8 text-sm">
+                Login
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );

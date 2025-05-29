f8fa0ec45aef030f260462e3c84a008d0dd29a6d
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-05-29 20:24:30 +0530

    initial commit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -17,6 +17,12 @@ const Header = () => {
         Unleash your creativity with AI. Turn your imagination into visual art
         in seconds - with just a few words.
       </p>
+
+      <button className="sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full">
+        Ganarate Images
+        <img className="h-6" src={assets.star_group} />
+      </button>
+      <div>{Array(6)}</div>
     </div>
   );
 };

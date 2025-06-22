763da692e10239e9ef6596bd0575f91faa64e397
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-06-22 20:57:19 +0530

    result page design

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 const Result = () => {
-  return <div>Result</div>;
+  return (
+    <>
+      <div>
+        <div className="relative">
+          <img src={assets.sample_img_1} alt="" className="max-w-sm rounded" />
+          <span className="absolute bottom-0 left-0 h-1" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Result;
